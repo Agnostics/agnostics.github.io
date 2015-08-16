171ca9264c27ad82e8ea2509c3a6157a8c88cc5e
Brandon <brandilla1337@gmail.com>
2015-08-16 01:58:56 -0400

    Added filter, and various online status.
    Now you can filter from online, offline, or all users. Online users now
    show live viewer count along with a preview.

--- js/main.js
+++ js/main.js
@@ -1,12 +1,34 @@
 var app = angular.module('twitch', []);
 
-names = ["kingagnostic", "freecodecamp", "summit1g", "GoldGlove", "LIRIK", "Giantwaffle", "GamesDoneQuick", "Voyboy", "2mgovercsquared", "RiotGames", "Shortyyguy", "Sinqnew", "Halo", "unitytech", "PhantomL0rd"];
+names = ["kingagnostic", "freecodecamp", "summit1g", "GoldGlove", "LIRIK", "Giantwaffle", "GamesDoneQuick", "Voyboy", "2mgovercsquared", "RiotGames", "Shortyyguy", "C9Sneaky", "Halo", "unitytech", "PhantomL0rd"];
 
 app.controller('body', function ($scope, $http) {
 
   $scope.allUsers = [];
   $scope.onlineUsers = [];
   $scope.offlineUsers = [];
+
+  $scope.data = {
+    availableOptions: [{
+      id: '1',
+      name: 'All Users',
+      array: $scope.allUsers
+    }, {
+      id: '2',
+      name: 'Online Users',
+      array: $scope.onlineUsers
+    }, {
+      id: '3',
+      name: 'Offline Users',
+      array: $scope.offlineUsers
+
+    }],
+    selectedOption: {
+      id: '1',
+      name: 'All Users',
+      array: $scope.allUsers
+    }
+  };
 
   angular.forEach(names, function (item) {
     $.getJSON('https://api.twitch.tv/kraken/channels/' + item + '?client_id=5j0r5b7qb7kro03fvka3o8kbq262wwm&callback=?').success(function (data) {
@@ -18,29 +40,31 @@ app.controller('body', function ($scope, $http) {
       obj.views = data.views;
 
       if (data.logo === null) {
-        obj.logo = "http://blog.ramboll.com/fehmarnbelt/wp-content/themes/ramboll2/images/profile-img.jpg";
+        obj.logo = "img/profile-img.jpg";
       } else {
         obj.logo = data.logo;
       }
 
       $.getJSON('https://api.twitch.tv/kraken/streams/' + item + '?client_id=5j0r5b7qb7kro03fvka3o8kbq262wwm&callback=?').success(function (data) {
+
         if (data.stream == null) {
           obj.online = false;
           $scope.offlineUsers.push(obj);
         } else {
           obj.online = true;
+          obj.viwers = data.stream.viewers;
+          obj.previewImg = data.stream.preview.large;
+          obj.gameName = data.stream.channel.game;
+          obj.gameStatus = data.stream.channel.status;
           $scope.onlineUsers.push(obj);
-        }
 
-        $scope.userDisplay = $scope.allUsers;
+        }
 
         $scope.$apply();
 
       });
 
       $scope.allUsers.push(obj);
-
-      $scope.$apply();
 
     });
 
